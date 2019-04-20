8474044406f4e851797eab52c77f6c2a6dcdafce
ijiangtao <ijiangtao@foxmail.com>
2019-04-20 21:13:32 +0800

    test

--- maven-devops/src/main/java/net/ijiangtao/tech/maven/web/controller/GitCommitController.java
+++ maven-devops/src/main/java/net/ijiangtao/tech/maven/web/controller/GitCommitController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.*;
+import java.util.*; 
 
 import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
 
